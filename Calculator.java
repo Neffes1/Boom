424f8bfb82fcb7879ad702a2de26fcd6ed14ae2e
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2025-02-19 18:06:19 +0300

    Create Calculator.java
    Первая точка

---
+++ Calculator.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+// Model - обработка математических выражений
+class CalculatorModel {
+    public double evaluate(String expression) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < expression.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if (eat('*')) x *= parseFactor();
+                    else if (eat('/')) x /= parseFactor();
+                    else if (eat('/') && eat('/')) x = Math.floor(x / parseFactor()); // // - деление без остатка
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor();
+                if (eat('-')) return -parseFactor();
+
+                double x;
+                int startPos = this.pos;
+                if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(expression.substring(startPos, this.pos));
+                } else if (eat('(')) {
+                    x = parseExpression();
+                    eat(')');
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor());
+
+                return x;
+            }
+        }.parse();
+    }
+}
+
+// View - пользовательский интерфейс
+class CalculatorView {
+    public String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите математическое выражение: ");
+        return scanner.nextLine();
+    }
+
+    public void displayResult(double result) {
+        System.out.println("Результат: " + result);
+    }
+}
+
+// Controller - управление
+class CalculatorController {
+    private CalculatorModel model;
+    private CalculatorView view;
+
+    public CalculatorController(CalculatorModel model, CalculatorView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void calculate() {
+        String expression = view.getInput();
+        try {
+            double result = model.evaluate(expression);
+            view.displayResult(result);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorController controller = new CalculatorController(model, view);
+
+        controller.calculate();
+    }
+}
